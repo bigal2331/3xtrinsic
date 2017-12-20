19080b87c23102455be52766e7386f9cf2d250d2
jnoriega <bigal2331@yahoo.com>
2017-12-20 19:56:30 +0000

    fixed msg duplication bug

--- src/App.js
+++ src/App.js
@@ -11,52 +11,51 @@ import setUserListAction from './actions/setUserList.js'
 import setCurrentUserAction from './actions/setCurrentUser.js'
 import { bindActionCreators } from 'redux'
 import './styles/main.css';
+import io from 'socket.io-client';
 
-// import './App.css';
 
-
+const socket = io('https://converse-app-jnoriega.c9users.io:8081');
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    socket.on('result', (translatedMsg) =>this.handleNewMsg(translatedMsg));
+  }
 
-  
-    //gets the initial list of users and sets the state's active users list
-    componentWillMount(){
-        
-        this.props.socket.on('share_user_list', (response) =>{
-          console.log('this is the response')
+  handleNewMsg(msg){
+      this.props.sendMsgActionPassedToProps(msg)
+  }
+
+  componentDidMount(){
+    socket.on('share_user_list', (response) =>{
           this.props.setUserListActionPassedToProps(response.users)
-        });
-    }
+    });
+  }
   
-
     //adds current user to the state and transmits it to the server
-    addUserToChat(name,language) {
-      
+  addUserToChat(name,language) {
         const {
           setCurrentUserActionPassedToProps,
         } = this.props;
         let newUser = {id:this.props.usersInTheStorePassedToProps.length , name, language, };
+
         setCurrentUserActionPassedToProps(newUser);
         this.props.addUserActionPassedToProps(newUser);
-        this.props.socket.emit('new_user_added',{newUser});
-    }
+        socket.emit('new_user_added',{newUser});
+  }
 
 
     //submits the new message and adds it to the chat locally
-    handleSubmit(event) {
+  handleSubmit(event) {
         event.preventDefault();
         this.props.sendMsgActionPassedToProps({
           sender:"Me",
           translation:this.props.msgSavedInTheStorePassedToProps.message}
         );
-        this.props.socket.emit('chat message',
+        socket.emit('chat message',
           {author:this.props.currentUserInTheStorePassedToProps,
           msg: this.props.msgSavedInTheStorePassedToProps.message}
         );
-          
-        this.props.socket.once('result', (translatedMsg) =>{
-          this.props.sendMsgActionPassedToProps(translatedMsg)
-        });
         this.props.clearMsgActionPassedToProps();
     }
     
@@ -103,28 +102,25 @@ class App extends Component {
     }
   }
 
+  const mapStateToProps = (state) => {
+    console.log('this is the state', state)
+      return {
+          msgSavedInTheStorePassedToProps: state.msgStateInTheStore,
+          usersInTheStorePassedToProps: state.userListInTheStore.users,
+          currentUserInTheStorePassedToProps: state.userListInTheStore.currentUser,
+      };
+  };
 
-    const mapStateToProps = (state) => {
-      console.log('this is the state', state)
-        return {
-            msgSavedInTheStorePassedToProps: state.msgStateInTheStore,
-            usersInTheStorePassedToProps: state.userListInTheStore.users,
-            currentUserInTheStorePassedToProps: state.userListInTheStore.currentUser,
-        };
-    };
-
-    const mapDispatchToProps = (dispatch) => {
-        return {
-            
-            
-            setMsgActionPassedToProps: bindActionCreators(setMsgAction, dispatch),
-            sendMsgActionPassedToProps: bindActionCreators(sendMsgAction, dispatch),
-            clearMsgActionPassedToProps: bindActionCreators(clearMsgAction, dispatch),
-            addUserActionPassedToProps: bindActionCreators(addUserAction, dispatch),
-            setCurrentUserActionPassedToProps: bindActionCreators(setCurrentUserAction, dispatch),
-            setUserListActionPassedToProps: bindActionCreators(setUserListAction, dispatch),
-        };
-    };
+  const mapDispatchToProps = (dispatch) => {
+      return {
+          setMsgActionPassedToProps: bindActionCreators(setMsgAction, dispatch),
+          sendMsgActionPassedToProps: bindActionCreators(sendMsgAction, dispatch),
+          clearMsgActionPassedToProps: bindActionCreators(clearMsgAction, dispatch),
+          addUserActionPassedToProps: bindActionCreators(addUserAction, dispatch),
+          setCurrentUserActionPassedToProps: bindActionCreators(setCurrentUserAction, dispatch),
+          setUserListActionPassedToProps: bindActionCreators(setUserListAction, dispatch),
+      };
+  };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import addUserReducer from './reducers/addUserReducer.js';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import io from 'socket.io-client';
 
 
 const allReducersCombined = combineReducers({
@@ -17,8 +16,7 @@ const allReducersCombined = combineReducers({
     
 });
 
-const socket = io('https://converse-app-jnoriega.c9users.io:8081');  
-
+           
               
 const store = createStore(allReducersCombined, applyMiddleware(thunk));
 
@@ -26,7 +24,7 @@ const store = createStore(allReducersCombined, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
-        <App socket={socket}/>
+        <App/>
     </Provider>, 
     document.getElementById('root'));
 registerServiceWorker();
